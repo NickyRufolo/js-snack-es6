0b5c35ac59f869c3b1d99bd6669f7d68a914f641
NikiRufolo <nikirufolo@gmail.com>
2022-11-08 17:02:46 +0100

    destructoring

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 let arrayNew; //creaiamo nuovo array che conterra i numeri compresi tra 2 e 4
-myArray.forEach((index) => { //con for eaxh vai nel myarray e PER OGNI index => mettimi nel arrey New i valori di myArray.slice gli index tra 2 e 4
+myArray.forEach((index) => { //con for each vai nel myarray e PER OGNI index => mettimi nel arrey New i valori di myArray.slice gli index tra 2 e 4
 arrayNew = myArray.slice(2,4);
 });
 
@@ -56,4 +56,73 @@ console.log(studentiId);
 
 
 
+//Snack 3 array di oggetti
+const bycicle = [{
+    name: 'bmx',
+    weight: 20
+},
+{
+    name: 'mountain-bike',
+    weight: 15
+},
+{
+    name: 'competition-bike',
+    weight: 13
+    }
+];
 
+//versione breve
+var min = Math.min(...bycicle.map(item => item.weight));
+console.log(min);
+
+//versione lunga
+var min = Math.min(...bycicle.map((item) => {
+    return item.weight
+}));
+console.log(min);
+
+
+//Snack 4
+const team = [{
+    name: 'Napoli',
+    points: 0,
+    fouls: 0
+},
+{
+    name: 'Inter',
+    points: 0,
+    fouls: 0
+},
+{
+    name: 'Chealse',
+    points: 0,
+    fouls: 0
+},
+{
+    name: 'Barcelona',
+    points: 0,
+    fouls: 0
+},
+];
+//generare numeri random nelle proprieta points e fouls
+
+function randomNum(min,max) { {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+}
+console.log(randomNum(10,100));
+
+
+//Ficcare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+
+team.forEach((element, index) => {
+    return element.points = randomNum(10,100), element.fouls = randomNum(10,100);      //ritornami nel fouls e points del dato element il random num 
+});
+    console.log(team);
+
+
+//Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+team.forEach(({points, fouls}, index) => {
+    console.log(points, fouls)
+});
